a74b6e4a803c5a0437c39dbcdfc15a732d7972e9
Beknazar <110666044+RazanBek@users.noreply.github.com>
2022-11-08 13:11:00 +0600

    hw8-admin
    hw8-admin

--- new_project/main/admin.py
+++ new_project/main/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from main.models import Film, Director
-# Register your models here.
 
-admin.site.register(Film)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = 'title director regiser rating dlitelnost created updated'.split()
+    search_fields = 'title'.split()
+    list_filter = 'director regiser rating created updated'.split()
+    list_editable = 'director regiser rating'.split()
+
+
+admin.site.register(Film, ProductAdmin)
 admin.site.register(Director)
--- new_project/main/models.py
+++ new_project/main/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 
 class Director(models.Model):
+    class Meta:
+        verbose_name = 'Директора'
+        verbose_name_plural = 'Директоры'
     name = models.CharField(max_length=150)
 
     def __str__(self):
@@ -13,13 +16,17 @@ class Director(models.Model):
 class Film(models.Model):
     class Meta:
         ordering = ['-rating', 'title', 'updated']
-    director = models.ForeignKey(Director, on_delete=models.PROTECT, related_name="directors", null=True)
-    title = models.CharField(max_length=100, default="no name")
-    regiser = models.CharField(max_length=150)
-    rating = models.FloatField()
-    dlitelnost = models.IntegerField()
-    created = models.DateTimeField(auto_now_add=True, null=True)
-    updated = models.DateTimeField(auto_now=True, null=True)
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
+    director = models.ForeignKey(Director, on_delete=models.PROTECT,
+                                 related_name="directors", null=True,
+                                 verbose_name='Директор')
+    title = models.CharField(max_length=100, default="no name", verbose_name='Название')
+    regiser = models.CharField(max_length=150, verbose_name='Режисер')
+    rating = models.FloatField(verbose_name='Рейтинг')
+    dlitelnost = models.IntegerField(verbose_name='Длительность')
+    created = models.DateTimeField(auto_now_add=True, null=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, null=True, verbose_name='Дата изменения')
 
     def __str__(self):
         return self.title
